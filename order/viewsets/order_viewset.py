68ed3bd8474a1aefd62c039d057f6aca5382e71f
Angelo-pdr <angelo.ppedrosa@gmail.com>
2024-01-30 12:26:21 -0300

    test build 4

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -2,6 +2,8 @@ from order.models import Order
 from order.serializers import OrderSerializer
 
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class OrderViewSets(ModelViewSet):
     serializer_class = OrderSerializer
